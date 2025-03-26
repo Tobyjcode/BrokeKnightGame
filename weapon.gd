47b3d0057727e671acdd16382306f2609020ad3a
Tobyjcode <tobias-jes@hotmail.com>
2025-03-26 11:03:41 +0100

    - Added a slash sprite to the weapon for visual feedback during attacks. - Did so when you use the weapon slash effect sprite gets shown

--- weapon.gd
+++ weapon.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var sprite = $Sprite2D
 @onready var animation_player = $AnimationPlayer
+@onready var slash_sprite = $SlashSprite
 
 var is_attacking = false
 var attack_cooldown = 0.2
@@ -10,12 +11,15 @@ var can_attack = true
 func _ready():
 	# Initialize at reset position
 	animation_player.play("RESET")
+	# Hide slash sprite initially
+	if slash_sprite:
+		slash_sprite.hide()
 
 func _process(_delta):
 	if Input.is_action_just_pressed("attack"):
 		attack()
 	
-	# Update weapon direction based on player direction
+	# Update weapon and slash effect direction based on player direction
 	var parent = get_parent()
 	if parent and parent.has_method("is_player"):
 		if parent.animated_sprite.flip_h:
@@ -24,17 +28,34 @@ func _process(_delta):
 		else:
 			scale.x = 1   # Normal scale when player faces right
 			position.x = 8  # Hold on right side
+			
+	# Update slash effect position during attack
+	if is_attacking and slash_sprite:
+		slash_sprite.rotation = sprite.rotation
 
 func attack():
 	if can_attack and not is_attacking:
 		is_attacking = true
 		can_attack = false
 		
-		# Play the attack animation
+		# Show and start slash effect
+		if slash_sprite:
+			slash_sprite.show()
+			slash_sprite.play("slash")
+			# Make slash follow sword rotation
+			slash_sprite.rotation = sprite.rotation
+		
+		# Play the weapon attack animation
 		animation_player.stop()
 		animation_player.play("attack")
 		
+		# Wait for animation to finish
 		await animation_player.animation_finished
+		
+		# Hide slash effect
+		if slash_sprite:
+			slash_sprite.hide()
+			
 		is_attacking = false
 		await get_tree().create_timer(attack_cooldown).timeout
 		can_attack = true
